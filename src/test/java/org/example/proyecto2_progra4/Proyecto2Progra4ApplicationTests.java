af9baa1cd92468d5b9db7a23adb9a9f232299473
AlejandroVV26 <alejandro.vargas.vargas@est.una.ac.cr>
2024-05-06 12:04:00 -0600

    Initial Commit

---
+++ src/test/java/org/example/proyecto2_progra4/Proyecto2Progra4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.proyecto2_progra4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto2Progra4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
