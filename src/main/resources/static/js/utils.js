cb78cde44018e8f6db455fe02312546d5dd9d019
DixonBm03 <dixon.bustos.medina@est.una.ac.cr>
2024-05-17 10:05:15 -0600

    ya no se cae pero algunos tabs no renderiza

--- src/main/resources/static/js/utils.js
+++ src/main/resources/static/js/utils.js
@@ -1,50 +1,39 @@
 function renderMenu(user) {
-    let menu = `
-        <div class="tabs">
-            <a href="/home" class="tab">
-                <img src="/images/casa.svg" alt="">
-                <label>Home</label>
-            </a>
-            <a href="/profile" class="tab">
-                <img src="/images/perfil.svg" alt="">
-                <label>Perfil</label>
-            </a>
-    `;
-
+    let menu = `<div class="tabs">
+    <a href="#" onclick="navigateTo('/home'); return false;" class="tab">
+      <img src="/images/casa.svg" alt="">
+      <label>Home</label>
+    </a>
+    <a href="#" onclick="navigateTo('/profile'); return false;" class="tab">
+      <img src="/images/perfil.svg" alt="">
+      <label>Perfil</label>
+    </a>`;
 
     if (user === 'admin') {
-         menu += `
-            <a href="/accesos" class="tab">
-                <img src="/images/accesos.svg" alt="">
-                <label>Accesos</label>
-            </a>
-        `;
+        menu += `<a href="#" onclick="navigateTo('/accesos'); return false;" class="tab">
+      <img src="/images/accesos.svg" alt="">
+      <label>Accesos</label>
+    </a>`;
     } else if (user === 'proveedor') {
-        menu += `
-            <a href="/clients" class="tab">
-                <img src="/images/accesos.svg" alt="">
-                <label>Clientes</label>
-            </a>
-            <a href="/products" class="tab">
-                <img src="/images/productos.svg" alt="">
-                <label>Productos</label>
-            </a>
-            <a href="/invoices" class="tab">
-                <img src="/images/declarar.svg" alt="">
-                <label>Facturar</label>
-            </a>
-            
-        `;
-
+        menu += `<a href="#" onclick="navigateTo('/clients'); return false;" class="tab">
+      <img src="/images/accesos.svg" alt="">
+      <label>Clientes</label>
+    </a>
+    <a href="#" onclick="navigateTo('/products'); return false;" class="tab">
+      <img src="/images/productos.svg" alt="">
+      <label>Productos</label>
+    </a>
+    <a href="#" onclick="navigateTo('/invoices'); return false;" class="tab">
+      <img src="/images/declarar.svg" alt="">
+      <label>Facturar</label>
+    </a>`;
     }
 
-    menu += `
-            <a href="/" class="tab">
-                <img src="/images/door-closed.svg" alt="">
-                <label>Logout</label>
-            </a>
-        </div>
-    `;
+    menu += `<a href="#" onclick="navigateTo('/'); return false;" class="tab">
+    <img src="/images/door-closed.svg" alt="">
+    <label>Logout</label>
+  </a>
+</div>`;
 
     return menu;
 }
@@ -52,12 +41,10 @@ function renderMenu(user) {
 function getUser() {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-
     if (email === 'admin@admin.com' && password === 'admin') {
         return 'admin';
     } else if (email === 'proveedor@proveedor.com' && password === 'proveedor') {
         return 'proveedor';
     }
-
     return null;
 }
