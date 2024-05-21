1e8c2b2e69f6ada4e2d3fb681947cc5b13308a97
AlejandroVV26 <alejandro.vargas.vargas@est.una.ac.cr>
2024-05-21 15:02:20 -0600

    Create ProveedorClienteDao.java

---
+++ src/main/java/org/example/proyecto2_progra4/data/ProveedorClienteDao.java
@@ -0,0 +1,48 @@
+package org.example.proyecto2_progra4.data;
+
+import org.example.proyecto2_progra4.logic.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class ProveedorClienteDao {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private ClienteDao clienteDao;
+
+    public void create(String provId, String clientId) throws Exception{
+        String sql="insert into ProveedorCliente (id_proveedor, id_cliente) values (?,?)";
+        try{
+            jdbcTemplate.update(sql, provId, clientId);
+        } catch (DataAccessException e){
+            throw new Exception("Error al crear ProveedorCliente: "+e.getMessage());
+        }
+    }
+
+    public List<Cliente> clientesPorProveedor(String provId){
+        String sql="select * from ProveedorCliente where id_proveedor=?";
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+            Cliente client = new Cliente();
+            try {
+                client=clienteDao.read(rs.getString("id_cliente"));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            return client;
+        });
+    }
+
+    public void delete(String provId, String clientId) throws Exception{
+        String sql="delete from ProveedorCliente where id_proveedor=? and id_cliente=?";
+        try{
+            jdbcTemplate.update(sql, provId, clientId);
+        }catch (DataAccessException e){
+            throw new Exception("Error al eliminar registro en ProveedorCliente: "+e.getMessage());
+        }
+    }
+}
