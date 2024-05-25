dc0fabe6c88fb4187b88601e6927bd68fbfc4e20
AlejandroVV26 <alejandro.vargas.vargas@est.una.ac.cr>
2024-05-24 23:57:28 -0600

    Update ProveedorController.java
    Apis para insertar y actualizar datos del proveedor

--- src/main/java/org/example/proyecto2_progra4/controller/ProveedorController.java
+++ src/main/java/org/example/proyecto2_progra4/controller/ProveedorController.java
@@ -1,4 +1,37 @@
 package org.example.proyecto2_progra4.controller;
 
+import org.example.proyecto2_progra4.data.ProveedorRepository;
+import org.example.proyecto2_progra4.logic.Proveedor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/proveedores")
 public class ProveedorController {
+    @Autowired
+    private ProveedorRepository proveedorRepository;
+
+    @PostMapping("/agergar")
+    public ResponseEntity<Void> addProveedor(Proveedor prov){
+        try{
+            proveedorRepository.create(prov);
+            return ResponseEntity.ok().build();
+        } catch (Exception e){
+            throw new RuntimeException("Error al agregar proveedor",e);
+        }
+    }
+
+    @PutMapping("/editar")
+    public ResponseEntity<Void> updateProv(Proveedor prov){
+        try{
+            proveedorRepository.update(prov);
+            return ResponseEntity.ok().build();
+        } catch (Exception e){
+            throw new RuntimeException("Error al actualizar datos del proveedor",e);
+        }
+    }
 }
