fcc6c47b61ce3955de36be3aef120382f3cf4705
Dylan <dylanpc25@gmail.com>
2024-05-25 19:41:03 -0600

    avance factura

--- src/main/java/org/example/proyecto2_progra4/controller/FacturaController.java
+++ src/main/java/org/example/proyecto2_progra4/controller/FacturaController.java
@@ -1,4 +1,39 @@
 package org.example.proyecto2_progra4.controller;
 
+import org.example.proyecto2_progra4.data.ClienteRepository;
+import org.example.proyecto2_progra4.data.FacturaRepository;
+import org.example.proyecto2_progra4.logic.Cliente;
+import org.example.proyecto2_progra4.logic.Factura;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/invoices")
 public class FacturaController {
+    @Autowired
+    private FacturaRepository facturaRepository;
+
+    @GetMapping("/{proveedorId}")
+    public ResponseEntity<List<Factura>> getProvFactura(@PathVariable("proveedorId") String provId) {
+        try {
+            List<Factura> list = facturaRepository.readByProveedor(provId);
+            return ResponseEntity.ok(list);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @PutMapping("/agregar")
+    public ResponseEntity<Void> agregarFactura(Factura factura) {
+        try {
+            facturaRepository.create(factura);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            throw new RuntimeException("Error al agregar factura", e);
+        }
+    }
 }
--- src/main/resources/static/js/invoices.js
+++ src/main/resources/static/js/invoices.js
@@ -7,17 +7,23 @@ function invoices() {
             ${renderMenu('proveedor')}
         </div>
         <section class="content">
-            <form>
+            <form onsubmit="Confirmacion()">
             <div class="input_par">
               <div class="input_comp">
                 <label for="clientId">ID del Cliente</label>
-                <input type="text" id="clientId" placeholder="ID del Cliente" required>
+                <select id="clientId" class = "form-select">
+                </select>
               </div>
               <div class="input_comp">
-                <label for="total">Total</label>
-                <input type="number" id="total" placeholder="Total de la factura" required>
+                <br><label for="total">Total</label>
+                <input type="number" id="total" placeholder="Total de la factura">
               </div>
             </div>
+            <div class="input_comp">
+                <label for="productos">Productos</label>
+                <select id="producto" class = "form-select">
+                </select>
+              </div>
             <div class="input_par">
               <div class="input_comp">
                 <label for="date">Fecha</label>
@@ -28,4 +34,76 @@ function invoices() {
           </form>
         </section>
     `;
+}
+
+function Confirmacion()  {
+    return `
+        <header>
+            <h1>Facturar</h1>
+        </header>
+        <div>
+            ${renderMenu('proveedor')}
+        </div>
+        <section class="content">
+            <form onsubmit="registrarFactura(event)">
+            <div class="input_par">
+              <div class="input_comp">
+                <label for="clientId">ID del Cliente</label>
+                <select id="clientId" class = "form-select">
+                </select>
+              </div>
+              <div class="input_comp">
+                <br><label for="total">Total</label>
+                <input type="number" id="total" placeholder="Total de la factura">
+              </div>
+            </div>
+            <div class="input_comp">
+                <label for="productos">Productos</label>
+                <select id="producto" class = "form-select">
+                </select>
+              </div>
+            <div class="input_par">
+              <div class="input_comp">
+                <label for="date">Fecha</label>
+                <input type="date" id="date" required>
+              </div>
+            </div>
+            <input class="button_submit" type="submit" value="Facturar">
+          </form>
+        </section>
+    `;
+}
+
+function registrarFactura(event) {
+    event.preventDefault(); // Evitar el envío del formulario
+    const prov = document.getElementById('proveedor').value;
+    const client = document.getElementById('ClienteId').value;
+    const costo = document.getElementById('cost').value;
+
+    const factura = {
+        id:id,
+        prov: prov,
+        client: client,
+        costoTotal:costo,
+    };
+
+    fetch(`/api/invoices/agregar`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(factura)
+    })
+        .then(response => {
+            if (response.ok) {
+                // Manejo de respuesta exitosa
+                alert('Factura registrada correctamente');
+            } else {
+                // Manejo de error
+                throw new alert('Error al registrar la factura');
+            }
+        })
+        .catch(error => {
+            alert('Error:', error);
+        });
 }
